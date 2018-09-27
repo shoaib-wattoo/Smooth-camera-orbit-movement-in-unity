e50120ca456bd1cfb9767238256c9919f4d50d21
DESKTOP-KF35RC4\ASUS <shoaib.wattoo@gmail.com>
2018-09-27 17:41:52 +0500

    pushing code

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+#region Delegates
+#endregion
+public class CameraController : MonoBehaviour {
+
+	#region Private References      
+    [SerializeField, Range(0.0f, 1.0f)]
+    private float _lerpRate;
+    private float _xRotation;
+    private float _yYRotation;
+
+	#endregion
+
+	#region Private Methods
+    private void Rotate(float xMovement, float yMovement)
+    {
+        _xRotation += xMovement;
+        _yYRotation += yMovement;
+    }
+	#endregion
+
+	#region Unity CallBacks
+	
+	void Start ()
+	{
+        InputManager.MouseMoved += Rotate;
+	}
+	
+	void Update ()
+	{
+        _xRotation = Mathf.Lerp(_xRotation, 0, _lerpRate);
+        _yYRotation = Mathf.Lerp(_yYRotation, 0, _lerpRate);
+        transform.eulerAngles += new Vector3(0, _xRotation, -_yYRotation);
+
+	}
+
+   void OnDestroy()
+   {
+       InputManager.MouseMoved += Rotate;    
+   }
+	#endregion
+}
---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+#region Delegates
+public delegate void MouseMoved(float xMovement, float yMovement);
+#endregion
+public class InputManager : MonoBehaviour
+{
+    #region Private References
+
+    private float _xMovement;
+    private float _yMovement;
+    #endregion
+
+    #region Events
+    public static event MouseMoved MouseMoved;
+    #endregion
+
+    #region Event Invoker Methods
+    private static void OnMouseMoved(float xmovement, float ymovement)
+    {
+        var handler = MouseMoved;
+        if (handler != null) handler(xmovement, ymovement);
+    }
+    #endregion
+
+    #region Private Methods
+
+    private void InvokeActionOnInput()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            _xMovement = Input.GetAxis("Mouse X");
+            _yMovement = Input.GetAxis("Mouse Y");
+            OnMouseMoved(_xMovement, _yMovement);
+        }
+    }
+    #endregion
+
+    #region Unity CallBacks
+   
+    void Update()
+    {
+        InvokeActionOnInput();
+    }
+
+    #endregion
+
+
+}
